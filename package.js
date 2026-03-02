bb24514d2d4164871ed8deeb4947e0b3f7b7ab5c
Amrit-Raj01 <amritrajsingh951@gmail.com>
2026-03-02 15:04:35 +0530

    Create package.json for file upload application
    Initialized package.json for the file upload app with dependencies and scripts.

---
+++ package.js
@@ -0,0 +1,19 @@
+{
+  "name": "file-upload-app",
+  "version": "1.0.0",
+  "description": "Full-stack file upload application",
+  "main": "app.js",
+  "scripts": {
+    "start": "node app.js",
+    "dev": "nodemon app.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "mongoose": "^7.6.3",
+    "multer": "^1.4.5-lts.1",
+    "cors": "^2.8.5"
+  },
+  "devDependencies": {
+    "nodemon": "^3.0.1"
+  }
+}
